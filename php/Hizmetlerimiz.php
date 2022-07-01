3e7888cca7a649b86e199535f9409170b38c6a3d
ömer berat gönülcü <ogonulcu@gmail.com>
2022-07-01 11:22:37 +0300

    ilk commit yapıldı

---
+++ php/Hizmetlerimiz.php
@@ -0,0 +1,37 @@
+<?php
+//$Sayfa = "Hakkımızda";
+include("../inc/head.php");
+include("../inc/db.php");
+
+$sorgu = $baglanti->prepare("select * from hizmetlerimiz");
+$sorgu->execute();
+$sonuc = $sorgu->fetch();
+
+?>
+        <!-- Page Header-->
+        <header class="masthead" style="background-image: url('assets/img/about-bg.jpg')">
+            <div class="container position-relative px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-md-10 col-lg-8 col-xl-7">
+                        <div class="page-heading">
+                            <h2> <?php echo $sonuc['ustBaslik'] ?> </h2>
+                            <span class="subheading"> <?php echo $sonuc['altBaslik'] ?></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- Main Content-->
+        <main class="mb-4">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <?php
+                    echo $sonuc['paragraf']
+                    ?>
+                </div>
+                </div>
+            </div>
+        </main>
+<?php
+include("../inc/footer.php");
+?>
